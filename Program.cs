17480f9720ac6014f654365969dd93a869efcfcb
Mike Chu <michael.chu@optimizely.com>
2024-10-28 12:49:39 -0400

    test: initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using OptimizelySDK;
+using OptimizelySDK.Config;
+using OptimizelySDK.Entity;
+
+DotNetEnv.Env.Load();
+var sdkKey = Environment.GetEnvironmentVariable("SDK_KEY_WITH_200_FLAGS");
+
+var configManager = new HttpProjectConfigManager.Builder()
+    .WithSdkKey(sdkKey)
+    .Build(false); // sync mode
+
+var optimizelyInstance = OptimizelyFactory.NewDefaultInstance(configManager);
+
+var attributes = new UserAttributes
+{
+    { "accountIdGuid", "mike-chu-testing" },
+    { "ring", "us" }
+};
+var output = optimizelyInstance.CreateUserContext("user123", attributes).DecideAll();
+
+Console.WriteLine($"{output.Count} decisions made at {DateTime.Now}");
