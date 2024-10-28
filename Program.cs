48edca4df1961a2f8194e8ecee9c739172f7fe1b
Mike Chu <michael.chu@optimizely.com>
2024-10-28 15:11:14 -0400

    test: naive approach to run 1000 tests

--- Program.cs
+++ Program.cs
@@ -2,20 +2,23 @@
 using OptimizelySDK.Config;
 using OptimizelySDK.Entity;
 
-DotNetEnv.Env.Load();
-var sdkKey = Environment.GetEnvironmentVariable("SDK_KEY_WITH_200_FLAGS");
-
-var configManager = new HttpProjectConfigManager.Builder()
-    .WithSdkKey(sdkKey)
-    .Build(false); // sync mode
-
-var optimizelyInstance = OptimizelyFactory.NewDefaultInstance(configManager);
-
-var attributes = new UserAttributes
+for (int i = 0; i < 1000; i++)
 {
-    { "accountIdGuid", "mike-chu-testing" },
-    { "ring", "us" }
-};
-var output = optimizelyInstance.CreateUserContext("user123", attributes).DecideAll();
+    DotNetEnv.Env.Load();
+    var sdkKey = Environment.GetEnvironmentVariable("SDK_KEY_WITH_200_FLAGS");
 
-Console.WriteLine($"{output.Count} decisions made at {DateTime.Now}");
+    var configManager = new HttpProjectConfigManager.Builder()
+        .WithSdkKey(sdkKey)
+        .Build(false); // sync mode
+
+    var optimizelyInstance = OptimizelyFactory.NewDefaultInstance(configManager);
+
+    var attributes = new UserAttributes
+    {
+        { "accountIdGuid", "mike-chu-testing" },
+        { "ring", "us" }
+    };
+    var output = optimizelyInstance.CreateUserContext("user123", attributes).DecideAll();
+
+    Console.WriteLine($"{output.Count} decisions made at {DateTime.Now}");
+}
